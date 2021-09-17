fca7409668eca2c341dbb8a725470a3d9408d16b
TERESIA012 <kingoriteresia@gmail.com>
2021-09-17 22:38:08 +0300

    add email configurations

--- config.py
+++ config.py
@@ -6,4 +6,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     
-    
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 525
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
