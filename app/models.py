ba1cb0f2b4f05cb071656eee6c6debb3afba7a1e
TERESIA012 <kingoriteresia@gmail.com>
2021-09-19 04:02:08 +0300

    modify signup

--- app/models.py
+++ app/models.py
@@ -1,143 +1,92 @@
-from datetime import datetime
-
-from flask_login import UserMixin, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-
-from app import db, login_manager
+from . import db
+from . import login_manager
+from flask_login import UserMixin
+from werkzeug import generate_password_hash,check_password_hash
 
 
+
+    
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), nullable=False, unique=True)
-    email = db.Column(db.String(255), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
-
-    def save(self):
+    username = db.Column(db.String(255), index=True, unique=True)
+   
+    bio = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255))
+    
+    def save_comment(self):
         db.session.add(self)
         db.session.commit()
-
-    def delete(self):
+    def delete_comment(self):
         db.session.delete(self)
         db.session.commit()
-
-    def set_password(self, password):
-        pass_hash = generate_password_hash(password)
-        self.password = pass_hash
-
-    def check_password(self, password):
-        return check_password_hash(self.password, password)
-
-    def __repr__(self):
-        return f'User: {self.username}'
-
-
-@login_manager.user_loader
-def user_loader(user_id):
-    return User.query.get(user_id)
-
-
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+    @password.setter
+    def password(self, password):
+            self.password_hash = generate_password_hash(password)
+    def verify_password(self,password):
+        return check_password_hash(self.password_hash,password)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+    
+    
 class Post(db.Model):
     __tablename__ = 'posts'
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    user_id = db.Column(db.String, nullable=False)
-    post = db.Column(db.String, nullable=False)
-    comment = db.relationship('Comment', backref='post', lazy='dynamic')
-    category = db.Column(db.String, nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-    up_vote = db.relationship('Upvote', backref='post', lazy='dynamic')
-    down_vote = db.relationship('Downvote', backref='post', lazy='dynamic')
-
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(255),nullable = False)
+    post = db.Column(db.Text(), nullable = False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category = db.Column(db.String(255), index = True,nullable = False)
     def save(self):
         db.session.add(self)
         db.session.commit()
-
     def delete(self):
         db.session.delete(self)
         db.session.commit()
-
     def __repr__(self):
-        return f"Post Title: {self.title}"
-
-
+        return f"Post('{self.title}', '{self.date_posted}')"    
+    
 class Comment(db.Model):
     __tablename__ = 'comments'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
-    comment = db.Column(db.Text())
-
-    def save(self):
+    id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.Text(), nullable = False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    upvote = db.relationship('Upvote',backref='comment',lazy='dynamic')
+    downvote = db.relationship('Downvote',backref='comment',lazy='dynamic')
+    def save_comment(self):
         db.session.add(self)
         db.session.commit()
-
-    @classmethod
-    def get_comments(cls, post_id):
-        comments = Comment.query.filter_by(post_id=post_id).all()
-        return comments
-
-    def delete(self):
+    def delete_comment(self):
         db.session.delete(self)
         db.session.commit()
-
     def __repr__(self):
-        return f'Comments: {self.comment}'
-
-
+        return f'Comment: {self.comment}'
+    
 class Upvote(db.Model):
     __tablename__ = 'upvotes'
-    id = db.Column(db.Integer, primary_key=True)
-    upvote = db.Column(db.Integer, default=1)
+    id = db.Column(db.Integer, primary_key = True)
+    upvote = db.Column(db.Integer, nullable = False)
+    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
-
-    def save(self):
+    def save_upvote(self):
         db.session.add(self)
         db.session.commit()
-
-    def upvote(cls, id):
-        upvote_post = Upvote(user=current_user, post_id=id)
-        upvote_post.save()
-
-    @classmethod
-    def query_upvotes(cls, id):
-        upvote = Upvote.query.filter_by(post_id=id).all()
-        return upvote
-
-    @classmethod
-    def all_upvotes(cls):
-        upvotes = Upvote.query.order_by('id').all()
-        return upvotes
-
+    def delete_upvote(self):
+        db.session.delete(self)
+        db.session.commit()
     def __repr__(self):
-        return f'{self.user_id}:{self.post_id}'
-
-
+        return f'Upvote: {self.upvote}'
+    
 class Downvote(db.Model):
-    __tablename__ = 'downvotes'
-    id = db.Column(db.Integer, primary_key=True)
-    downvote = db.Column(db.Integer, default=1)
+    __tablename__= 'downvotes'
+    id = db.Column(db.Integer, primary_key = True)
+    downvote = db.Column(db.Integer, nullable = False)
+    Comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'))
-
-    def save(self):
-        db.session.add(self)
-        db.session.commit()
-
-    def downvote(cls, id):
-        downvote_post = Downvote(user=current_user, post_id=id)
-        downvote_post.save()
-
-    @classmethod
-    def query_downvotes(cls, id):
-        downvote = Downvote.query.filter_by(post_id=id).all()
-        return downvote
-
-    @classmethod
-    def all_downvotes(cls):
-        downvote = Downvote.query.order_by('id').all()
-        return downvote
-
-    def __repr__(self):
-        return f'{self.user_id}:{self.post_id}'
